6d3b6c5618e68feee4194d13d587e3d995f791a0
Diana <dicomeinit@gmail.com>
2022-07-20 19:07:07 +0300

    Retrieving the Insiteblog entries from MongoDB

--- app.py
+++ app.py
@@ -6,27 +6,22 @@ import certifi
 app = Flask(__name__)
 client = MongoClient("mongodb+srv://diana:diana12345@insiteblog-application.7n6bk.mongodb.net/test", tlsCAFile=certifi.where())
 app.db = client.insiteblog
-entries = []
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    print([e for e in app.db.entries.find({})])
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.append((entry_content, formatted_date))
         app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
-    entries_with_date = [
-        (
-            entry[0],
-            entry[1],
-            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
-         )
-        for entry in entries
-
-    ]
+    entries_with_date = []
+    for entry in app.db.entries.find({}):
+        entries_with_date.append((
+            entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+        ))
     return render_template("home.html", entries=entries_with_date)
 
 
